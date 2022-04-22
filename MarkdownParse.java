8b601bda5502b0e4aa8a55935fda337746bce1f3
michellem8 <mmazo@ucsd.edu>
2022-04-22 08:45:20 -0700

    commands
    javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
    java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -32,6 +32,7 @@ public class MarkdownParse {
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
+        
 
         System.out.println("hello");
     }
